0a1915529ad95330ce5b9a6846db7c583516a5b7
Kennashka DeSilva <mail@kennashka.com>
2019-09-03 12:39:28 -0400

    Update exercise-2-6.cpp

--- exercise-2-6.cpp
+++ exercise-2-6.cpp
@@ -1,1 +1,28 @@
 
+//include statement(s)
+
+//using namespace statement
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+//variable declaration
+
+    string name;
+    double studyHours;
+
+//executable statements
+cout << "Enter your name: " << endl;  
+cin >> name;
+
+cout << "Enter your study hours: " << endl;  
+cin >> studyHours;
+
+    cout << "Hello, " << name <<"! On Saturday, you need to study " << studyHours <<" hours for the exam." << endl;
+
+//return statement
+
+    return 0;
+}
