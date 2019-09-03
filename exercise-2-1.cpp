8e38330ed7d3f031ea4f3f8a4a1eceb6b27d4a6f
Kennashka DeSilva <mail@kennashka.com>
2019-09-03 12:21:38 -0400

    Create exercise-2-1.cpp

---
+++ exercise-2-1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Write your main here
+    // 
+    // JS: cosole.log( "Programming Assignment 1")
+    // JS: cosole.log( "Computer Programming I")
+    // JS: cosole.log( " Author: ??? ")
+    // JS: cosole.log( "Due Date: Thursday, Jan. 24")
+    // 
+ cout <<"**********************************" << endl;
+   cout << "*    Programming Assignment 1    *" << endl;
+   cout << "*    Computer Programming I      *" << endl;
+   cout << "*    Author: Kennashka DeSilva   *" << endl;
+   cout << "*    Due Date: Thursday, Jan. 24 *" << endl;
+ cout <<"**********************************" << endl;
+    return 0;
+}
