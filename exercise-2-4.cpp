2bf302751a2d36f7874d9f663987c020dee4bbcd
Kennashka DeSilva <mail@kennashka.com>
2019-09-03 12:36:57 -0400

    Update exercise-2-4.cpp

--- exercise-2-4.cpp
+++ exercise-2-4.cpp
@@ -1,1 +1,29 @@
 
+//include statement(s)
+#include <iostream>
+//using namespace statement
+using namespace std;
+
+int main()
+{
+double num1;
+    double num2;
+    double num3;
+    double average;
+//variable declaration
+ num1 = 75.35 ;
+ num2 = -35.56;
+ num3 = 15.76;
+ 
+ average = (num1 + num2 + num3 ) / 3;
+
+//executable statements
+cout << "num1 is "<< num1 << endl;
+cout << "num2 is "<< num2 << endl;
+cout << "num3 is "<< num3 << endl;
+cout << "average is "<< average << endl;
+
+//return statement
+
+    return 0;
+}
