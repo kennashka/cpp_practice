bdc42ce1cace374c72ec7350e2c9fb32b6897b3f
Kennashka DeSilva <mail@kennashka.com>
2019-09-03 12:37:56 -0400

    Update exercise-2-5.cpp

--- exercise-2-5.cpp
+++ exercise-2-5.cpp
@@ -1,1 +1,22 @@
 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+double radius;
+double area;
+
+const double PI = 3.14;
+double circumference;
+     
+cout << "Enter the radius: ";
+cin >> radius;
+cout << endl;
+    
+circumference = 2 * PI * radius;
+area = PI * radius * radius;
+    
+cout << "Area = " << area << endl;
+
+cout << "Circumference = " << circumference << endl;
