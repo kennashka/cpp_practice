1daf0c3578230593f9a9efe60a66e495bf11c11a
Kennashka DeSilva <mail@kennashka.com>
2019-09-03 12:47:21 -0400

    Update exercise-2-10.cpp

--- exercise-2-10.cpp
+++ exercise-2-10.cpp
@@ -1,1 +1,36 @@
 
+#include <iostream>
+# include <ctgmath>
+
+using namespace std;
+
+int main() {
+        double deci1;
+        double deci2;
+        double deci3;
+        double deci4;
+        double deci5;
+        double convert;
+
+ 
+cout << "Enter first decimal number: " << endl;
+cin >> deci1;
+    
+cout << "Enter second decimal number: " << endl;
+cin >> deci2;
+    
+cout << "Enter third decimal number: " << endl;
+cin >> deci3;
+    
+cout << "Enter fourth decimal number: " << endl;
+cin >> deci4;
+    
+cout << "Enter fifth decimal number: " << endl;
+cin >> deci5;
+    
+convert = deci1 + deci2 + deci3 + deci4 + deci5;
+    
+cout << "Sum of all decimals converted to the nearest integer: " << llround(convert) << endl;  
+    
+    return 0;
+}
