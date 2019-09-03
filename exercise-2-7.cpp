fe604366caffa0f5f5b57d901c671adee1b85e68
Kennashka DeSilva <mail@kennashka.com>
2019-09-03 12:41:11 -0400

    Update exercise-2-7.cpp

--- exercise-2-7.cpp
+++ exercise-2-7.cpp
@@ -1,1 +1,20 @@
 
+#include <iostream>
+#include <cmath> 
+#include <ctgmath>
+using namespace std;
+
+int main() {
+    
+    double num;
+        
+    // Write your main here
+   
+   cout << "Enter your number:" << endl;
+       cin >> num;
+   
+       cout << "Your number: " <<  llround(num) << endl;
+
+    
+    return 0;
+}
